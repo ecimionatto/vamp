349afcb479e0136ad6b912c889bdaac247c53dfa
edson.cimionatto <ecimionatto@gmail.com>
2020-05-21 16:39:33 -0400

    integrating traces

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+// New - import the React Router components, and the Profile page component
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <h1 className="text-center">Vamp</h1>
+        <NavBar />
+      </BrowserRouter>
     </div>
   );
 }
